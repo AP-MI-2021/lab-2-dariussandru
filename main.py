e52cf8ef3490a096563b938677e1211c0ddcc9dd
unknown <darius.sandru@stud.ubbcluj.ro>
2021-10-06 08:16:01 +0300

    Lab 3 gata

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def is_prime(nr):
     """
     if nr < 2:
         return False
-    for i in range(2,nr):
+    for i in range(2, nr):
         if nr % i == 0:
             return False
     return True
@@ -39,14 +39,14 @@ def is_superprime(x):
     """
     copie = x
     nr_cifre = 0
-    var=0
+    var = 0
     while copie > 9:
         nr_cifre += 1
         copie //= 10
     # acum in nr_cifre stim cate cifre are numarul x
     copie = x
     while nr_cifre >= 0:
-        cifra=copie // 10 ** nr_cifre
+        cifra = copie // 10 ** nr_cifre
         # in cifra avem prima cifra a numarului x
         if is_prime(cifra):
             print(f"{cifra} este pirm")
@@ -55,10 +55,30 @@ def is_superprime(x):
             var += 1
         nr_cifre -= 1
         # nr_cifra scade pentru a putea lua primele 2 cifre ale numarului x
+    print()
     if var >= 1:
-        print(f"Numarul  nu este super prim")
+        return False
     else:
-        print(f"Numarul  este super prim")
+        return True
+
+
+def nr_prim_mic(x):
+    '''
+
+    :param x: un nr int
+    :return: cel mai mare nr mai mic decat x care e prim
+    '''
+    i = 2
+    n = 0
+    for i in range(x):
+        if is_prime(i):
+            n = i
+    print(n)
+
+
+assert is_palindrome(121) == True
+assert is_prime(7) == True
+assert is_superprime(233) == True
 
 
 def main():
@@ -67,6 +87,7 @@ def main():
         print("  ")
         print("1 Palindrom")
         print("2 Super prim")
+        print("3 Nr prim mai mic ca x")
         print("x Iesire")
         print()
         optiune = input("Alege optiunea  ")
@@ -80,10 +101,18 @@ def main():
 
         if optiune == "2":
             x = int(input("Dati un nr :"))
-            is_superprime(x)
+            if is_superprime(x):
+                print(f"Nr {x} este super prim")
+            else:
+                print(f"Nr {x} nu este super prim")
+
+        if optiune == "3":
+            x = int(input("Dati un nr :"))
+            nr_prim_mic(x)
 
         if optiune == "x":
             print("Programul se inchide")
             f = True
+
 
 main()
